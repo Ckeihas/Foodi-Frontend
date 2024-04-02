e1180bbc598d61a385f133e787c4971d7eb930a2
Christian Keihäs <christiankeihas@Christian-MacBook-Pro.local>
2024-04-02 14:56:16 +0300

    Collection items pagination

--- src/components/homeScreen/favourites/CollectionItems.tsx
+++ src/components/homeScreen/favourites/CollectionItems.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text, TouchableOpacity, ActivityIndicator, FlatList } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, ActivityIndicator, FlatList, Dimensions, SafeAreaView } from "react-native";
 import { Ionicons, MaterialCommunityIcons, Feather } from '@expo/vector-icons'; 
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -13,8 +13,11 @@ type CollectionProps = NativeStackScreenProps<FavoriteParams, 'collectionItems'>
 interface ICollectionPosts {
     post: {};
 }
+const { width, height } = Dimensions.get('window');
+
 const CollectionItems = ({route}: CollectionProps) => {
     const [collectionPosts, setCollectionPosts] = useState<ICollectionPosts[]>([]);
+    const [lastVisibleItem, setLastVisibleItem] = useState<string>();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const navigation = useNavigation<NativeStackNavigationProp<FavoriteParams>>()
@@ -25,17 +28,20 @@ const CollectionItems = ({route}: CollectionProps) => {
         }
         setLoading(true)
         try {
-            const response = await axios.post(url, {items: route.params.itemPaths})
+            const getFirstBatch = route.params.itemPaths.slice(0,15)
+            const response = await axios.post(url, {items: getFirstBatch})
             const { items, error } = response.data
             if(error){
                 if(await CreateNewAccessToken()){
                     try {
-                        const response = await axios.post(url, {items: route.params.itemPaths})
+                        const getFirstBatch = route.params.itemPaths.slice(0,15)
+                        const response = await axios.post(url, {items: getFirstBatch})
                         const { items, error } = response.data
                         if(error){
                             setError(true)
                         } else {
-                            setCollectionPosts(items)
+                            setCollectionPosts(items)       
+                            setLastVisibleItem(getFirstBatch[getFirstBatch.length - 1])
                         }
                     } catch (error) {
                         console.log("Error fetching collections posts: ", error)
@@ -46,7 +52,9 @@ const CollectionItems = ({route}: CollectionProps) => {
                     setError(true)
                 }
             } else {
+                const getFirstBatch = route.params.itemPaths.slice(0,15)
                 setCollectionPosts(items)
+                setLastVisibleItem(getFirstBatch[getFirstBatch.length - 1])
             }
             setLoading(false)
         } catch (error) {
@@ -56,24 +64,75 @@ const CollectionItems = ({route}: CollectionProps) => {
         }
     };
 
+    const PaginateNextPage = async () => {
+        try {
+            if(lastVisibleItem){
+                const findLastItem = route.params.itemPaths.indexOf(lastVisibleItem)
+                if(findLastItem !== -1){
+                    const nextBatch = route.params.itemPaths.slice(findLastItem + 1);
+                    const next15Batch = nextBatch.slice(0, 15)
+
+                    const response = await axios.post(url, {items: next15Batch})
+                    const { items, error } = response.data
+                    if(error){
+                        if(await CreateNewAccessToken()){
+                            try {
+                                const response = await axios.post(url, {items: next15Batch})
+                                const { items, error } = response.data
+                                if(error){
+                                    setError(true)
+                                } else {
+                                    setCollectionPosts((existingItems) => {
+                                        return [...existingItems, ...items]
+                                    })          
+                                    setLastVisibleItem(next15Batch[next15Batch.length - 1])
+                                }
+                            } catch (error) {
+                                console.log("Error fetching collections posts: ", error)
+                                setError(true);
+                            }
+                        } else {
+                            console.log("Error when creating new access token favorite screen: ", error)
+                            setError(true)
+                        }
+                    } else {
+                        setCollectionPosts((existingItems) => {
+                            return [...existingItems, ...items]
+                        })
+                        setLastVisibleItem(next15Batch[next15Batch.length - 1])
+                    }
+                }
+            }
+            
+        } catch (error) {
+            setError(true);
+            setLoading(false)
+            console.log("error occured: ", error)
+        }
+        console.log("next page")
+    };
+
     useEffect(() => {
         GetItemsFromCollection();
     }, [])
 
     return(
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             { loading ? <ActivityIndicator /> :
             <>
-            <TouchableOpacity onPress={() => navigation.goBack()}>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.header}>
                 <Ionicons name="chevron-back-outline" size={30} color="gray" />
             </TouchableOpacity>       
             <FlatList 
             data={collectionPosts}
             renderItem={({item}) => <RenderPostItem item={item}/>}
+            contentContainerStyle={styles.flatlistStyles}
+            onEndReached={() => PaginateNextPage()}
+            onEndReachedThreshold={1}
             />
             </>
         }
-        </View>
+        </SafeAreaView>
     )
 };
 
@@ -84,5 +143,13 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    header: {
+        alignSelf: 'flex-start',
+        marginTop: 10,
+        marginHorizontal: 20
+    },
+    flatlistStyles: {
+        width: width
     }
 })
--- src/components/homeScreen/favourites/RenderPostItem.tsx
+++ src/components/homeScreen/favourites/RenderPostItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
+import { AntDesign, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 
 interface IPostItem {
     post: any
@@ -7,7 +8,35 @@ interface IPostItem {
 const RenderPostItem = ({item}: {item: IPostItem}) => {
     return(
         <View style={styles.container}>
-            <Text>{item.post.title}</Text>
+            <Image source={{uri: item.post.imageURL}} style={styles.img}/>
+            <View style={styles.postInfo}>
+                <Text style={styles.titleText}>{item.post.title}</Text>
+                <Text style={styles.descriptionText}>
+                    {
+                        item.post.description.length > 100 ? item.post.description.slice(0,100) + "..." : item.post.description
+                    }
+                </Text>
+
+                <View style={styles.macrosCont}>
+                            <View style={styles.timeCont}>
+                                <AntDesign name="clockcircleo" size={10} color="#40D94E" />
+                                <Text style={styles.cardText}>20m</Text>
+                            </View>
+                            <View style={styles.calorCont}>
+                                <FontAwesome5 name="fire" size={10} color="#40D94E" />
+                                <Text style={styles.cardText}>550kcla</Text>
+                            </View>
+                            <View style={styles.calorCont}>
+                                <MaterialCommunityIcons name="arm-flex-outline" size={10} color="#40D94E" />
+                                <Text style={styles.cardText}>16g</Text>
+                            </View>
+                            <View style={styles.calorCont}>
+                                <FontAwesome5 name="weight" size={10} color="#40D94E" />
+                                <Text style={styles.cardText}>10g</Text>
+                            </View>
+                        </View>
+            </View>
+            
         </View>
     )
 };
@@ -16,8 +45,45 @@ export default RenderPostItem;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+        marginVertical: 30,
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        height: 120,
+        
+    },
+    img: {
+        width: 100,
+        height: 120,
+        borderRadius: 20
+    },
+    postInfo: {
+        marginVertical: 10,
+        marginLeft: 10,
+        justifyContent: 'space-between'
+    },
+    titleText: {
+        fontWeight: 'bold'
+    },
+    descriptionText: {
+        width: 250
+    },
+    macrosCont: {
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
+    cardText: {
+        color: 'black',
+        fontSize: 12
+    },
+    timeCont: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginRight: 5
+    },
+    calorCont: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 5,
+        marginRight: 5
+    },
 });
